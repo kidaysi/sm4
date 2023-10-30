96649601b4c45dc8f67383568b915d398436f357
kidaysi <frvrevrlana@gmail.com>
2023-10-30 17:51:06 +0300

    sm4

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+include('classes/car.php');
+include('lib/car.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+<br><br><br>
+<div class="container">
+    <?php
+    $car = new Car();
+    $namespaceCar = new \lib\Car;
+    ?>
+</div>
+</body>
+
+</html>
